09b72f8e3cd63d32a88df872013e1951d94da697
CodeByBlazej <codebyblazej@gmail.com>
2024-06-19 19:31:35 +0100

    add code solving cipher

--- app.rb
+++ app.rb
@@ -1,5 +1,27 @@
 def caesar_cipher (string, shift_factor)
-  alphabet = ('a'..'b').to_a + ('0'..'9').to_a
+  alphabet = ('a'..'z').to_a + ('0'..'9').to_a
   array = string.chars
-  array.map {|char| char.replace(alphabet.index * 8)}
+  string_alphabet_index = []
+
+  array.each do |char|
+    string_alphabet_index.push(alphabet.index(char))
+  end
+
+  shifted_array = string_alphabet_index.map {|char| char + shift_factor}
+  
+  caesar_string = []
+
+  shifted_array.each do |char|
+    caesar_string.push(alphabet.at(char))
+  end
+
+
+  p string_alphabet_index
+  p shifted_array
+  p caesar_string.join('')
 end
+
+# make version where I store index of array in another array
+# and then compare them to alphabet
+
+caesar_cipher('kupka', 5)
