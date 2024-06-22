bfe63a203a6e54c4178cfe4ccdbc014d4f8e1512
CodeByBlazej <codebyblazej@gmail.com>
2024-06-22 15:30:42 +0100

    solve cipher loop from z to a

--- app.rb
+++ app.rb
@@ -2,33 +2,16 @@ def caesar_cipher (string, shift_factor)
   alphabet = ('a'..'z').to_a
   # alphabet = /\w/
   array = string.chars
+  # array_with_no_nums = /^(\d) (.*)$/
   string_alphabet_index = []
 
   array.each do |char|
     string_alphabet_index.push(alphabet.index(char))
-    # if regex then:
-    # string_alphabet_index.push(alphabet.match(char))
   end
 
-  if (string_alphabet_index.index)
-  shifted_array = string_alphabet_index.map do |idx|
-    i = 0
-    until i >= shift_factor do
-    idx +  
 
-# shifted_array = []
+  shifted_array = string_alphabet_index.map {|idx| (idx + shift_factor) % alphabet.length}
 
-#   string_alphabet_index.each do |char|
-#     result = char + shift_factor
-#     if (result <= alphabet.length)
-#       shifted_array.push(result)
-#     elsif (result > alphabet.length)
-#       result
-#       shifted_array.push()
-#     end
-#   end
-
-#   string_alphabet_index.map do |char|
     
 
 
@@ -39,12 +22,13 @@ def caesar_cipher (string, shift_factor)
   end
 
 
+
+
   p string_alphabet_index
   p shifted_array
   p caesar_string.join('')
 end
 
-# make version where I store index of array in another array
-# and then compare them to alphabet
 
-caesar_cipher('kuwopkazuuu', 5)
+
+caesar_cipher('abcdefgzzz', 5)
