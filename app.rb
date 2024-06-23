1fb7ec3a425a88dba6bf51eb1556d7a2e65c1c7f
CodeByBlazej <codebyblazej@gmail.com>
2024-06-23 21:43:45 +0100

    solve cipher without upcases

--- app.rb
+++ app.rb
@@ -2,37 +2,45 @@ require 'pry-byebug'
 
 def caesar_cipher (string, shift_factor)
   alphabet = ('a'..'z').to_a
-  oryg_string = string.downcase.chars
   array = string.chars
-  array_with_no_nums = /^(\d) (\s) (.*)$/
   string_alphabet_index = []
 
 
   array.each do |char|
-    if alphabet.include?(char.downcase) ## try with || here for upcase
+    if alphabet.include?(char.downcase)
       string_alphabet_index.push(alphabet.index(char.downcase))
     else
       string_alphabet_index.push(char)
     end
   end
 
+  
+  shifted_array = string_alphabet_index.map {|idx| idx.class == Integer ? (idx + shift_factor) % alphabet.length : idx}
+  
+    
+  caesar_string = []
+  
+  shifted_array.each do |char|
+    if char.is_a? Integer
+      caesar_string.push(alphabet.at(char))
+    else
+      caesar_string.push(char) 
+    end
+  end
+  
   binding.pry
 
-  shifted_array = string_alphabet_index.map {|idx| idx != nil ? (idx + shift_factor) % alphabet.length : idx}
-
-  # shifted_array = []
-
-
-
-
-  caesar_string = []
-
-  shifted_array.each do |char|
-    caesar_string.push(alphabet.at(char))
-  end
+  # array.each_with_index do |char, index|
+  #   if (char =~ /[A-Z]/) == 0
+  #     caesar_string.at(index).replace(char.upcase)
+  
+  #   end
+  # end
 
 
 
+
+  p array
   p string_alphabet_index
   p shifted_array
   p caesar_string.join('')
@@ -40,4 +48,4 @@ end
 
 
 
-caesar_cipher('aBBbcdefgzzz ! @ # 123', 15)
+caesar_cipher('aBBbbcdefgzzzbbb ! @ # 123', 15)
